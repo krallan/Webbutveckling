6f68596de41d7adaa8a9131619e4ed541ab2ca46
krallan <linusandrenilsson@hotmail.se>
2017-10-10 16:27:56 +0200

    loi90olö

---
+++ Klient/Gesäll/header.php
@@ -0,0 +1,10 @@
+<header class="mainHeader">
+    <span class="logoHeader"><a href="index.php">Logo</a></span>
+    <nav>
+        <a href="">Vapen</a>
+        <a href="">Attachments</a>
+        <a href="">Jämför</a>
+        <a href="" class="pullRight">Logga in</a>
+    </nav>
+
+</header>
